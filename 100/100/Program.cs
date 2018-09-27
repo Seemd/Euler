8ffe40c731f06cbbee727f51f288b22c81495454
Seemd <43569944+Seemd@users.noreply.github.com>
2018-09-27 16:57:13 +0100

    Add files via upload

---
+++ 100/100/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int amount = int.Parse(Console.ReadLine());
+            for (int i = 0; i > amount;)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    List<int> intList = new List<int>();
+                    intList.Add(i);
+                }
+            }
+            
+            
+
+            
+        }
+    }
+}
